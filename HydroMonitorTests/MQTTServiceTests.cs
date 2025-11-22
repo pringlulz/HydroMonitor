4cac000549bca249e9cbb519cfeed8560b9a684e
zmey <zmey@DESKTOP-5BLFJV9>
2025-11-22 15:43:04 -0500

    Added unit tests; Fixes issues with compilation related to adding unit tests and the release of .NET 10. (thanks Microsoft!)

---
+++ HydroMonitorTests/MQTTServiceTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HydroMonitor.Services;
+
+namespace HydroMonitorTests
+{
+    internal class MQTTServiceTests
+    {
+    }
+}
